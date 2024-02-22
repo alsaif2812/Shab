94449e609d4a94a36aa7160572332bedd4e2f227
Mohammad Al Saif <alsaifsiddiqui1@gmail.com>
2024-02-22 10:49:58 +0530

    a

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,24 +1,32 @@
 'use client';
+import React from 'react';
+import Head from 'next/head';
 import './globals.css';
-import 'remixicon/fonts/remixicon.css'
+import 'remixicon/fonts/remixicon.css';
 import { Nunito } from 'next/font/google';
-import Navbar  from '@/components/Navbar';
+import Navbar from '@/components/Navbar';
 
 const nunitoResult = Nunito({
     subsets: ['latin'],
     display: 'swap',
 });
 
-
 export default function RootLayout({ children }) {
     return (
         <html lang="en" className={`${nunitoResult.className} select-none`}>
-            <body >
+            <Head>
+                <meta charSet="UTF-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <meta name="description" content="SHab-e-Barat" />
+
+                <title>SHab-e-Barat</title>
+            </Head>
+            <body>
                 <div>
                     <Navbar />
                     {children}
                 </div>
             </body>
-        </html >
+        </html>
     );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,15 @@
-
+import data from '@/textContent/faq/roman.json'
+import FaqQuestions from '@/components/FaqQuestions'
+export const metadata = {
+    title: 'Shab-e-Barat '
+};
 export default function Home() {
     return (
         <>
-
+            <Head>
+                <title>{metadata.title}</title>
+            </Head>
+            <FaqQuestions FAQCardData={data} />
         </>
     );
 }
